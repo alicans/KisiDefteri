180c478e48006747be1472b5756c026400c031ca
alicans <alican@outlook.com>
2023-10-09 14:11:49 +0300

    tasarımların oluşturulması

--- KisiDefteri/Form1.Designer.cs
+++ KisiDefteri/Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtAd = new TextBox();
+            txtSoyad = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            btnDuzenle = new Button();
+            btnSil = new Button();
+            SuspendLayout();
+            // 
+            // txtAd
+            // 
+            txtAd.Location = new Point(12, 27);
+            txtAd.Name = "txtAd";
+            txtAd.Size = new Size(100, 23);
+            txtAd.TabIndex = 0;
+            // 
+            // txtSoyad
+            // 
+            txtSoyad.Location = new Point(118, 27);
+            txtSoyad.Name = "txtSoyad";
+            txtSoyad.Size = new Size(100, 23);
+            txtSoyad.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(118, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Soyad:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(224, 27);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Ekle >>";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 56);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(287, 304);
+            listBox1.TabIndex = 5;
+            // 
+            // btnDuzenle
+            // 
+            btnDuzenle.Location = new Point(12, 366);
+            btnDuzenle.Name = "btnDuzenle";
+            btnDuzenle.Size = new Size(100, 23);
+            btnDuzenle.TabIndex = 6;
+            btnDuzenle.Text = "Düzenle";
+            btnDuzenle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            btnSil.Location = new Point(118, 366);
+            btnSil.Name = "btnSil";
+            btnSil.Size = new Size(100, 23);
+            btnSil.TabIndex = 7;
+            btnSil.Text = "Sil";
+            btnSil.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(332, 415);
+            Controls.Add(btnSil);
+            Controls.Add(btnDuzenle);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtSoyad);
+            Controls.Add(txtAd);
+            Name = "Form1";
+            Text = "Kişi Defteri";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtAd;
+        private TextBox txtSoyad;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private ListBox listBox1;
+        private Button btnDuzenle;
+        private Button btnSil;
     }
 }
